704c9465b52ae5acb64d21d2cb94dbeb40d3d77a
Miguel Perez <migeperezjr@gmail.com>
2025-02-13 00:54:48 -0600

    Added multiplication function

--- calculator.py
+++ calculator.py
@@ -12,7 +12,12 @@ def subtract_numbers(num1, num2):
     return num1 - num2
 
 
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
 if __name__ == "__main__":
     print("This is a simple calculator.")
     print(f"Addition: 5 + 3 = {add_numbers(5, 3)}")
     print(f"Subtraction: 10 - 4 = {subtract_numbers(10, 4)}")
+    print(f"Multiplication: 6 * 3 = {multiply_numbers(6, 3)}")
