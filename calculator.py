083f1ca249b5a6ace480f8edec014b4d37ae0147
Katie Bridson <kbridson@memphis.edu>
2025-02-13 12:02:35 -0600

    Added divide functionality to calculator

--- calculator.py
+++ calculator.py
@@ -4,6 +4,7 @@ Shows how parallel development can lead to conflicts when
 multiple developers modify the same file.
 """
 
+
 def add_numbers(num1, num2):
     return num1 + num2
 
@@ -12,7 +13,16 @@ def subtract_numbers(num1, num2):
     return num1 - num2
 
 
+def divide_numbers(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        print("Error: cannot divide by zero")
+
+
 if __name__ == "__main__":
     print("This is a simple calculator.")
     print(f"Addition: 5 + 3 = {add_numbers(5, 3)}")
     print(f"Subtraction: 10 - 4 = {subtract_numbers(10, 4)}")
+    print(f"Divide: 10 / 2 = {divide_numbers(10, 2)}")
+    print(f"Divide: 10 / 0 = {divide_numbers(10, 0)}")
